6defdbbdc4ea409e145f8a0f2fc33634c4a3f836
romanoaraujo-srijant <romano.araujo@srijan.net>
2022-10-18 11:27:01 +0530

    login UI implemented

--- app/src/main/java/com/experiment/foodproductapp/MainActivity.kt
+++ app/src/main/java/com/experiment/foodproductapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.experiment.foodproductapp.constants.Screen
 import com.experiment.foodproductapp.ui.theme.FoodProductAppTheme
+import com.experiment.foodproductapp.views.SignInPage
 import com.experiment.foodproductapp.views.SignupPage
 import com.experiment.foodproductapp.views.SplashScreenPage
 
@@ -26,6 +27,9 @@ class MainActivity : ComponentActivity() {
                     }
                     composable(route = Screen.SignUpScreen.route) {
                         SignupPage()
+                    }
+                    composable(route = Screen.SignInScreen.route) {
+                        SignInPage()
                     }
                 }
             }
--- app/src/main/java/com/experiment/foodproductapp/constants/Screen.kt
+++ app/src/main/java/com/experiment/foodproductapp/constants/Screen.kt
@@ -2,5 +2,6 @@ package com.experiment.foodproductapp.constants
 
 sealed class Screen(val route:String){
     object SplashScreen : Screen("SplashScreen")    // Splash Screen
-    object SignUpScreen : Screen("SignUpScreen")    // SignUp Screen
+    object SignUpScreen : Screen("SignUpScreen")
+    object SignInScreen : Screen("SignInScreen")// SignUp Screen
 }
