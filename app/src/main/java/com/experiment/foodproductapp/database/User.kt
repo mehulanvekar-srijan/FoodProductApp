ab6d6cf1e3e7d959caa0d574b853e5606fb7d069
mehulanvekar-srijan <mehul.anvekar@srijan.net>
2022-10-19 11:58:01 +0530

    new attributes added in User class

--- app/src/main/java/com/experiment/foodproductapp/database/User.kt
+++ app/src/main/java/com/experiment/foodproductapp/database/User.kt
@@ -8,9 +8,11 @@ import java.util.*
 @Entity(indices = [ Index(value = ["userName"], unique = true) ])
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    val userName: String,
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val password: String,
+    var userName: String,
+    var firstName: String,
+    var lastName: String,
+    var email: String,
+    var password: String,
+    var dob: String,
+    var phoneNumber: String,
 )
