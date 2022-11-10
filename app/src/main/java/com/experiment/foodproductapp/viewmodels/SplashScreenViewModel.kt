c3d28e1b36a440911d4d1db6ffb66395360a64ac
mehulanvekar-srijan <mehul.anvekar@srijan.net>
2022-11-10 15:35:40 +0530

    dummy data bug solved

--- app/src/main/java/com/experiment/foodproductapp/viewmodels/SplashScreenViewModel.kt
+++ app/src/main/java/com/experiment/foodproductapp/viewmodels/SplashScreenViewModel.kt
@@ -10,6 +10,7 @@ import com.experiment.foodproductapp.constants.Screen
 import com.experiment.foodproductapp.database.OrderDetails
 import com.experiment.foodproductapp.repository.DatabaseRepository
 import kotlinx.coroutines.*
+import java.lang.Exception
 
 class SplashScreenViewModel : ViewModel() {
 
@@ -21,7 +22,6 @@ class SplashScreenViewModel : ViewModel() {
             delay(splashDuration)
 
             dummyData(context)
-
 
             val loggedInEmail: String? = DatabaseRepository(context).getLoggedInUser()
 
@@ -44,42 +44,45 @@ class SplashScreenViewModel : ViewModel() {
 
     private fun dummyData(context: Context) {
         viewModelScope.launch(Dispatchers.IO) {
-            DatabaseRepository(context).insertOrder(
-                OrderDetails(
-                    email = "romi@romi.com",
-                    id = 0,
-                    url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
-                    title = "Coolberg Non Alcoholic Beer - Malt",
-                    description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
-                    price = 79,
-                    orderId = 1,
-                    canceled = false
+            try {
+                DatabaseRepository(context).insertOrder(
+                    OrderDetails(
+                        email = "romi@romi.com",
+                        id = 0,
+                        url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
+                        title = "Coolberg Non Alcoholic Beer - Malt",
+                        description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
+                        price = 79,
+                        orderId = 1,
+                        canceled = false
+                    )
                 )
-            )
-            DatabaseRepository(context).insertOrder(
-                OrderDetails(
-                    email = "romi@romi.com",
-                    id = 1,
-                    url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
-                    title = "Coolberg Non Alcoholic Beer - Mint",
-                    description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
-                    price = 79,
-                    orderId = 1,
-                    canceled = false,
+                DatabaseRepository(context).insertOrder(
+                    OrderDetails(
+                        email = "romi@romi.com",
+                        id = 1,
+                        url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
+                        title = "Coolberg Non Alcoholic Beer - Mint",
+                        description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
+                        price = 79,
+                        orderId = 1,
+                        canceled = false,
+                    )
                 )
-            )
-            DatabaseRepository(context).insertOrder(
-                OrderDetails(
-                    email = "romi@romi.com",
-                    id = 2,
-                    url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
-                    title = "Coolberg Non Alcoholic Beer - Cranberry",
-                    description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
-                    price = 79,
-                    orderId = 2,
-                    canceled = false
+                DatabaseRepository(context).insertOrder(
+                    OrderDetails(
+                        email = "romi@romi.com",
+                        id = 2,
+                        url = "https://products3.imgix.drizly.com/ci-budweiser-24269668d4e23c97.jpeg",
+                        title = "Coolberg Non Alcoholic Beer - Cranberry",
+                        description = "Coolberg Malt Beer is a Non-Alcoholic Beer. This NAB has toasty notes of barley malts and hops and a distinctive musky aroma. It is made from the finest natural barley malts extracts. It is carbonated and has a serious spunk. As it contains less carbonation and often develops a beer-like head when poured into a glass. It is a perfect blend of crisp, bold and smooth flavour. Enjoy it with your choice of snack in the evening or serve it at a party.",
+                        price = 79,
+                        orderId = 2,
+                        canceled = false
+                    )
                 )
-            )
+            }
+            catch (e: android.database.sqlite.SQLiteConstraintException){ }
         }
     }
 
