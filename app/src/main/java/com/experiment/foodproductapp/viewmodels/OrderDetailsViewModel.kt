cb3691d0911a7239c33515d26308283599433723
SahilDeosekar <102315985+SahilDeosekar@users.noreply.github.com>
2022-11-10 12:37:58 +0530

    orederdetails view and viewmodel files

---
+++ app/src/main/java/com/experiment/foodproductapp/viewmodels/OrderDetailsViewModel.kt
@@ -0,0 +1,5 @@
+package com.experiment.foodproductapp.viewmodels
+
+class OrderDetailsViewModel {
+
+}
---
+++ app/src/main/java/com/experiment/foodproductapp/views/OrderHistory.kt
@@ -0,0 +1,16 @@
+package com.experiment.foodproductapp.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.experiment.foodproductapp.viewmodels.HomeScreenViewModel
+import com.experiment.foodproductapp.viewmodels.OrderDetailsViewModel
+import com.experiment.foodproductapp.viewmodels.UserDetailsViewModel
+
+@Composable
+fun OrderDetails(
+    email : String?,
+    navHostControllerLambda: () -> NavHostController,
+    orderDetailsViewModel: OrderDetailsViewModel,
+){
+
+}
